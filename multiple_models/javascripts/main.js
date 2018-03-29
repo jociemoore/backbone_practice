91189d498347f90cb2ddc57b60990ab451b689ca
Jocelyn Moore <hello@jociemoore.com>
2018-03-28 20:08:17 -0400

    Add javascript files

---
+++ multiple_models/javascripts/main.js
@@ -0,0 +1,29 @@
+// create models for items_json with IDs
+
+// sortBy name using Underscore
+
+// render as table rows using Handlebars
+
+// create a click event for the delete link that calls remove()
+
+// remove() --> deletes the item by ID
+//              create new array w/o item using Underscore 
+//              set to collections array
+//              rerender items array
+
+// create a submit event for form
+
+// submit --> create new model
+//            add model to collections array
+//            render new table row
+//            reset form
+
+// create click event for the delete all link
+
+// delete all --> empty collection array
+//                re-render template
+
+
+// create click event for the table headings --> sorts and re-renders
+
+// create localStorage of collection array --> assign value to collection array on page load
